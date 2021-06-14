931ffbdbf04043f1172b4538228c791ae04b6cf9
bastawesy <mohamedel.bastawesy2020@gmail.com>
2021-06-14 14:15:47 +0200

    migrattion scripts handeled

--- app/infrastructure/config/databaseConfigurations.js
+++ app/infrastructure/config/databaseConfigurations.js
@@ -19,5 +19,15 @@ module.exports = {
     "port": process.env.PROD_DB_PORT,
     "dialect": process.env.PROD_DB_DIALICT,
     "migrationStorage": "json" // Use a different storage type. Default: sequelize
+  },
+  // didn't determined yet .
+  "local": {
+    "username": process.env.LOCAL_DB_USERNAME,
+    "password": process.env.LOCAL_DB_PASSWORD,
+    "database": process.env.LOCAL_DB_NAME,
+    "host": process.env.LOCAL_DB_HOST,
+    "port": process.env.LOCAL_DB_PORT,
+    "dialect": process.env.LOCAL_DB_DIALICT,
+    "migrationStorage": "json" // Use a different storage type. Default: sequelize
   }
 }
