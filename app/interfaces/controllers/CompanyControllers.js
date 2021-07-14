732047d89ce5778ace034efca91919bc0933ee74
bastawesy <mohamedel.bastawesy2020@gmail.com>
2021-07-14 16:34:11 +0200

    company 2

--- app/interfaces/controllers/CompanyControllers.js
+++ app/interfaces/controllers/CompanyControllers.js
@@ -78,7 +78,7 @@ module.exports = class{
     async getAllCompaniesWithRatesByCategoryId(req,res){
         try{
             console.log("##############getAllCompaniesWithRatesByCategoryId#########################")
-            const result= await this.companyServices.getAllCompaniesWithRatesByCategoryId(req.body.categoryId)
+            const result= await this.companyServices.getAllCompaniesWithRatesByCategoryId(1)//req.body.categoryId
              if(result.length == 0)
              return res.status(200).json(Response.format(200,req.polyglot.t('emptyrResponse'),result))
             return res.status(200).json(Response.format(200,req.polyglot.t('companyDetails'),result))
