4ac361fbcc36cd317e8cbc8aadd65e92114f5564
bastawesy <mohamedel.bastawesy2020@gmail.com>
2021-07-14 16:41:07 +0200

    make param commpany query

--- app/interfaces/routes/company.js
+++ app/interfaces/routes/company.js
@@ -11,6 +11,7 @@ companyRouters.get('/company/profile',companyControllers.getCompanyById)
 companyRouters.get('/company/company-services',companyControllers.getAllCompaniesByCategoryServiceId)
 companyRouters.get('/company/all-by-category',companyControllers.getAllCompaniesByCategoryId)
 companyRouters.get('/company/all-with-rates',companyControllers.getAllCompaniesWithRate)
-companyRouters.get('/company/all-by-category-with-rates',CompanyValidator.mobileUserValidate('allByCategoryWithRates'),procErr,companyControllers.getAllCompaniesWithRatesByCategoryId)
+companyRouters.get('/company/all-by-category-with-rates/:id',companyControllers.getAllCompaniesWithRatesByCategoryId)
+// companyRouters.get('/company/all-by-category-with-rates/:id',CompanyValidator.mobileUserValidate('allByCategoryWithRates'),procErr,companyControllers.getAllCompaniesWithRatesByCategoryId)
 
 module.exports = companyRouters
