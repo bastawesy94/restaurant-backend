42008b51261be480cdb3d35b4cb0a25b7baf1944
bastawesy <mohamedel.bastawesy2020@gmail.com>
2021-07-15 14:13:21 +0200

    company join query updated

--- app/infrastructure/repositories/CompanyRepositoryMySQL.js
+++ app/infrastructure/repositories/CompanyRepositoryMySQL.js
@@ -22,7 +22,7 @@ module.exports = class extends CompanyRepo{
     }
 
     getAllCompaniesByCategoryServiceId(categoryServiceId){
-            return this.db.sequelize.query('SELECT company.name , AVG(review.stars) as rating FROM company LEFT OUTER JOIN review ON review.company_id = company.id WHERE company.category_service_id= $1 GROUP BY company.name',{
+            return this.db.sequelize.query('SELECT company.name , AVG(review.stars) as rating FROM company LEFT JOIN review ON review.company_id = company.id WHERE company.category_service_id= $1 GROUP BY company.name',{
                 bind: [categoryServiceId],
                 model: this.db.Company,
                 type: QueryTypes.SELECT
@@ -36,7 +36,7 @@ module.exports = class extends CompanyRepo{
     }
 
     getAllCompaniesWithRatesByCategoryId(categoryId){
-        return this.db.sequelize.query('SELECT company.name , AVG(review.stars) as rating FROM company LEFT OUTER JOIN review ON review.company_id = company.id INNER JOIN category_service ON company.category_Service_id = category_service.id WHERE category_service.category_id=$1 GROUP BY company.name',{
+        return this.db.sequelize.query('SELECT company.name , AVG(review.stars) as rating FROM company LEFT JOIN review ON review.company_id = company.id INNER JOIN category_service ON company.category_Service_id = category_service.id WHERE category_service.category_id=$1 GROUP BY company.name',{
             bind: [categoryId],
             model: this.db.Company,
             type: QueryTypes.SELECT
@@ -44,8 +44,8 @@ module.exports = class extends CompanyRepo{
     }
 
     getAllCompaniesWithRate(){
-            return this.db.sequelize.query('SELECT company.name , AVG(review.stars) as rating FROM company LEFT OUTER JOIN review ON review.company_id = company.id GROUP BY company.name',{
-                model: this.db.Company,
+            return this.db.sequelize.query('SELECT company.name , AVG(review.stars) as rating FROM company LEFT JOIN review ON review.company_id = company.id GROUP BY company.name',{
+                // model: this.db.Company,
                 type: QueryTypes.SELECT
             })
     }
